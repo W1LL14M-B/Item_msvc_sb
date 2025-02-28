d148f7433c0373e0618dda1b474111729e1915e1
William <concejo@CB-20583917.CONCEJOBTA.LOC>
2025-02-28 17:44:30 -0500

    msvc intem

---
+++ src/main/java/com/example/william/springcloud/msvc/items/services/ItemServiceFeign.java
@@ -0,0 +1,43 @@
+package com.example.william.springcloud.msvc.items.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.william.springcloud.msvc.items.clients.ProductFeingClient;
+import com.example.william.springcloud.msvc.items.models.Item;
+import com.example.william.springcloud.msvc.items.models.Product;
+
+@Service
+public class ItemServiceFeign implements ItemService {
+
+@Autowired
+private ProductFeingClient client;
+
+@Override
+public List<Item> findAll() {
+    return client.findAll()
+    .stream()
+    .map(p -> new Item (p, new Random().nextInt(10) + 1))
+    .collect(Collectors.toList());
+}
+
+
+
+
+@Override
+public Optional<Item>findById(Long id) {
+Product product = client.details(id);
+if(product == null ) {
+    return Optional.empty();
+}
+
+    return Optional.ofNullable(new Item(client.details(id), new Random().nextInt(10) + 1));
+
+}
+
+}
